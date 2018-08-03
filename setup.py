1197b2bb45138c7e03cb2c321fa37f914e4b9189
Sculpting Evolution <sculptingevo@gmail.com>
2018-08-03 15:47:53 -0400

    auxpump original source

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='auxpump',
+    version='1.0',
+    packages=find_packages(),
+    license='MIT',
+    description='',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    url='https://github.com/dgretton/auxpump.git',
+    author='Dana Gretton',
+    author_email='dgretton@mit.edu'
+)
